c80b32cdb74511a5e1efdeecddc4c025cc58c867
[wuhuiyun07] <wuhuiyun07@gmail.com>
2021-09-03 14:33:27 -0400

    module add

--- src/plot.R
+++ src/plot.R
@@ -1,3 +1,14 @@
+
+module load intel/19.0.5
+module load R/3.6.2
+module load geos/3.8.0
+module load gdal-2.4.3/intel-19.0
+module load proj-5.2.0/intel-19.0
+module load udunits-2.2.26/intel-19.0
+
+
+
+
 library(devtools)
 library(dplyr)
 library(dygraphs)
