e6ce9c312a3f0be407f095db160a266fcb599728
Tom Purucker <purucker.tom@gmail.com>
2025-08-15 15:44:22 -0400

    update hpc imports for sims #56 for weekly flux

--- src/01load_hpc_data_local.R
+++ src/01load_hpc_data_local.R
@@ -36,40 +36,49 @@ src_dir <- file.path(of_root, "src")
 hpc_data <- file.path(of_root, "hpc_data", "sim56-flux-weekly")
 hpc_data_sensitivity <- file.path(of_root, "hpc_data", "sim56-sensitivity")
 
-# 0, 1, 5, 10, 15, 19
-# load parameters but in list format
+# simulation sets 0, 3, 6, 9, 11
+# load parameter fits but in list format
 # 18 parameter fits for each variable
+# set 0
 load(file.path(hpc_data, "fitted_parameters0.RData"))
 fitted_parameter_list0 <- fitted_parameter_list
 str(fitted_parameter_list)
-fitted_parameter_list0[[2]]$estimate
-fitted_parameter_list0[[5]]$estimate
-fitted_parameter_list0[[8]]$estimate
-fitted_parameter_list0[[18]]$estimate
+for(i in 1:18){
+  print(fitted_parameter_list0[[i]]$estimate)
+}
 
-load(file.path(hpc_data, "fitted_parameters1.RData"))
-fitted_parameter_list1 <- fitted_parameter_list
-fitted_parameter_list1[[2]]$estimate
+# set 3
+load(file.path(hpc_data, "fitted_parameters3.RData"))
+fitted_parameter_list3 <- fitted_parameter_list
+for(i in 1:18){
+  print(fitted_parameter_list3[[i]]$estimate)
+}
 
-load(file.path(hpc_data, "fitted_parameters4.RData"))
-fitted_parameter_list4 <- fitted_parameter_list
-fitted_parameter_list4[[2]]$estimate
+# set 6
+load(file.path(hpc_data, "fitted_parameters6.RData"))
+fitted_parameter_list6 <- fitted_parameter_list
+for(i in 1:18){
+  print(fitted_parameter_list6[[i]]$estimate)
+}
 
-load(file.path(hpc_data, "fitted_parameters7.RData"))
-fitted_parameter_list7 <- fitted_parameter_list
-fitted_parameter_list7[[2]]$estimate
+# set 9
+load(file.path(hpc_data, "fitted_parameters9.RData"))
+fitted_parameter_list9 <- fitted_parameter_list
+for(i in 1:18){
+  print(fitted_parameter_list9[[i]]$estimate)
+}
 
-load(file.path(hpc_data, "fitted_parameters10.RData"))
-fitted_parameter_list10 <- fitted_parameter_list
-fitted_parameter_list10[[2]]$estimate
-
-load(file.path(hpc_data, "fitted_parameters13.RData"))
-fitted_parameter_list13 <- fitted_parameter_list
-fitted_parameter_list13[[2]]$estimate
+# set 11
+load(file.path(hpc_data, "fitted_parameters11.RData"))
+fitted_parameter_list11 <- fitted_parameter_list
+for(i in 1:18){
+  print(fitted_parameter_list11[[i]]$estimate)
+}
 
 # this loads everything for the generation stats
-load(file.path(hpc_data, "generation_stats13.RData"))
-generation_stats <- generation_stats[1:14,]
+load(file.path(hpc_data, "generation_stats11.RData"))
+generation_stats <- generation_stats[1:11,]
+#View(generation_stats)
 
 # nse scores for each generation
 load(file.path(hpc_data, "nses_parameters0.RData"))
@@ -78,73 +87,62 @@ dim(nses_parameters0)
 parameter_names <- colnames(nses_parameters0)[5:22]
 parameter_names
 
-load(file.path(hpc_data, "nses_parameters1.RData"))
-nses_parameters1 <- nses_parameters
-dim(nses_parameters1)
+load(file.path(hpc_data, "nses_parameters3.RData"))
+nses_parameters3 <- nses_parameters
+dim(nses_parameters3)
 
-load(file.path(hpc_data, "nses_parameters4.RData"))
-nses_parameters4 <- nses_parameters
-dim(nses_parameters4)
+load(file.path(hpc_data, "nses_parameters6.RData"))
+nses_parameters6 <- nses_parameters
+dim(nses_parameters6)
 
-load(file.path(hpc_data, "nses_parameters7.RData"))
-nses_parameters7 <- nses_parameters
-dim(nses_parameters7)
+load(file.path(hpc_data, "nses_parameters9.RData"))
+nses_parameters9 <- nses_parameters
+dim(nses_parameters9)
 
-load(file.path(hpc_data, "nses_parameters10.RData"))
-nses_parameters10 <- nses_parameters
-dim(nses_parameters10)
-
-load(file.path(hpc_data, "nses_parameters13.RData"))
-nses_parameters13 <- nses_parameters
-dim(nses_parameters13)
+load(file.path(hpc_data, "nses_parameters11.RData"))
+nses_parameters11 <- nses_parameters
+dim(nses_parameters11)
 
 # parameter inputs for each generation
 load(file.path(hpc_data, "parameter_input_sims0.RData"))
 parameter_inputs0 <- parameter_input_sims
 dim(parameter_inputs0)
 
-load(file.path(hpc_data, "parameter_input_sims1.RData"))
-parameter_inputs1 <- parameter_input_sims
-dim(parameter_inputs1)
+load(file.path(hpc_data, "parameter_input_sims3.RData"))
+parameter_inputs3 <- parameter_input_sims
+dim(parameter_inputs3)
 
-load(file.path(hpc_data, "parameter_input_sims4.RData"))
-parameter_inputs4 <- parameter_input_sims
-dim(parameter_inputs4)
+load(file.path(hpc_data, "parameter_input_sims6.RData"))
+parameter_inputs6 <- parameter_input_sims
+dim(parameter_inputs6)
 
-load(file.path(hpc_data, "parameter_input_sims7.RData"))
-parameter_inputs7 <- parameter_input_sims
-dim(parameter_inputs7)
+load(file.path(hpc_data, "parameter_input_sims9.RData"))
+parameter_inputs9 <- parameter_input_sims
+dim(parameter_inputs9)
 
-load(file.path(hpc_data, "parameter_input_sims10.RData"))
-parameter_inputs10 <- parameter_input_sims
-dim(parameter_inputs10)
-
-load(file.path(hpc_data, "parameter_input_sims13.RData"))
-parameter_inputs13 <- parameter_input_sims
-dim(parameter_inputs13)
+load(file.path(hpc_data, "parameter_input_sims11.RData"))
+parameter_inputs11 <- parameter_input_sims
+dim(parameter_inputs11)
 
 # list of output
 load(file.path(hpc_data, "bac_cal0.RData"))
 bac_cal_output0 <- bac_cal_output
 dim(bac_cal_output0)
 
-load(file.path(hpc_data, "bac_cal1.RData"))
-bac_cal_output1 <- bac_cal_output
-dim(bac_cal_output1)
+load(file.path(hpc_data, "bac_cal3.RData"))
+bac_cal_output3 <- bac_cal_output
+dim(bac_cal_output3)
 
-load(file.path(hpc_data, "bac_cal4.RData"))
-bac_cal_output4 <- bac_cal_output
-dim(bac_cal_output4)
+load(file.path(hpc_data, "bac_cal6.RData"))
+bac_cal_output6 <- bac_cal_output
+dim(bac_cal_output6)
 
-load(file.path(hpc_data, "bac_cal7.RData"))
-bac_cal_output7 <- bac_cal_output
-str(bac_cal_output7)
+load(file.path(hpc_data, "bac_cal9.RData"))
+bac_cal_output9 <- bac_cal_output
+str(bac_cal_output9)
 
-load(file.path(hpc_data, "bac_cal10.RData"))
-bac_cal_output10 <- bac_cal_output
-dim(bac_cal_output10)
+load(file.path(hpc_data, "bac_cal11.RData"))
+bac_cal_output11 <- bac_cal_output
+str(bac_cal_output11)
 
-load(file.path(hpc_data, "bac_cal13.RData"))
-bac_cal_output13 <- bac_cal_output
-str(bac_cal_output13)
 
